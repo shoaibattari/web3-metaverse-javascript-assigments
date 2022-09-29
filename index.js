8cf5f1310b4a04d39cbe3c2f9ca41ea1fe6b61d7
shoaib memon <shoaibattari83@gmail.com>
2022-09-30 01:01:28 +0500

    add assigment

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("hello shoaib memon");
+
+var name = "shoaib memon"
+var name2 = prompt("enter name");
+alert(name);
+
+if(name === name2)
+{
+    alert("your name match")
+}
+else{
+    alert("your name not match")
+}
