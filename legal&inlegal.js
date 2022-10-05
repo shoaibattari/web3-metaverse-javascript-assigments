ec420d10ed5c4959fd4d80d61c9165a78ff5d2ec
shoaib memon <shoaibattari83@gmail.com>
2022-10-06 00:32:12 +0500

    legal&illegal

---
+++ legal&inlegal.js
@@ -0,0 +1,37 @@
+// 1. Declare 3 variables in one statement
+let a = 5;
+let b = 5;
+let c = 5;
+
+///2. Declare 5 legal & 5 illegal variable names.
+
+//legal
+// let book = "brother in the land";
+// let pub = "oup";
+// let pages = 255;
+// let price$ = 250;
+// let discount = 10;
+// // illegal
+// let book- = "brother in the land";
+// let title% = 'green';
+// let pages@ = 250;
+// let price# = 2500;
+// let discount% = 10;
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS vavrriables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _______
+
+
+let head = "<h1>Rules for naming JS variables</h1>  <br>";
+let $1 = "Variable names can only contain, numbers, $ and _. for example: $my_1stVariable <br>"
+let $2 = "Variables must begin with a letter, $ or _, For example $name, _name or name <br>"
+let $3 = "Variable names are case sensitive <br>"
+let $4 = "Variable names should not be JS keywords"
+document.write(head,$1,$2,$3,$4);
