6f24a3e3ed498e512f75e6ee4ba385079e863595
shoaib memon <shoaibattari83@gmail.com>
2022-10-09 22:54:12 +0500

    edit

---
+++ classwork/class1.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+
+
+var name1 = "shoaib memon";
+// alert(name);
+console.log(name1);
---
+++ classwork/class2.js
@@ -0,0 +1,13 @@
+ var name6 = "shoiab"
+ console.log(name6);
+
+ var name7 = ("shoiabsattar");
+  console.log(name7);
+
+  var name8 =  `shoaib
+  abdul
+  sattar`;
+  document.write(name8);
+  console.log(name8);
+
+  
