b9e40094ee666984f2d1270236f1fa172f0d2c95
shoaib memon <shoaibattari83@gmail.com>
2022-10-11 16:09:20 +0500

    formula edit

--- milesToKilmeter/index.js
+++ milesToKilmeter/index.js
@@ -1,7 +1,6 @@
 // create function converter miles to kilometer
 
-function mileTokms(converter){
+function mileTokms(){
 let userMile = document.getElementById("miles_user").value;
- converter = userMile*1.609344;
-document.getElementById("convert_value");
-convert_value.value = converter;};
+document.getElementById("convert_value").innerHTML = userMile*1.609344;};
+
