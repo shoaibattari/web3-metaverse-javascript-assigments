e20647aac8299a65ac9ebd72b2bdcc90a65fed9e
shoaib memon <shoaibattari83@gmail.com>
2022-10-11 11:11:24 +0500

    code clean

--- milesToKilmeter/index.js
+++ milesToKilmeter/index.js
@@ -3,14 +3,5 @@
 function mileTokms(converter){
 let userMile = document.getElementById("miles_user").value;
  converter = userMile*1.609344;
-
-// console.log(result);
-// document.write(converter);
-// console.log(userMile)
 document.getElementById("convert_value");
-let a = convert_value.value = converter;
-// alert("the distance of " + userMile + "kms is equal to " + converter + " miles")
-console.log(a);
-
-
-};
+convert_value.value = converter;};
