c722418494bc814b65d86db25d437a381860ce97
shoaib memon <shoaibattari83@gmail.com>
2022-11-01 21:44:50 +0500

    add classwork

---
+++ classwork/index.mjs
@@ -0,0 +1,67 @@
+//  const sub = (a,b)=>{
+    // return a-b;
+//  }
+ 
+//  const result = sub(5,4);
+//  console.log(result);
+
+//  const subt = (c,d)=> c-d;
+//  const result2 = subt(5,4);
+//  console.log(result2);
+
+
+// const greet = (name)=> console.log((`hello ${name}`));
+// const result3 = greet("saleem");
+// console.log(result3);
+// console.log(greet("Sattar"));
+
+// const arr = [1,2,3,4,];
+// const arr2 = arr
+// arr2[2] = 5; // both array value change
+// const arr3 = [...arr]
+// arr3[2]=10; // both array not value changes
+// console.log(arr);
+// console.log(arr2);
+// console.log(arr3);
+
+// this function return array 
+// const add=(...params)=>{
+    // console.log(params);};
+// const result = add(1,2,3,4,5);
+
+// this function auto call
+// (function(){
+//     console.log("hello world");
+// })();
+
+
+// function dosumthing(){
+//     console.log("hellooooo");
+// dosumthing();
+// }
+// dosumthing()
+
+
+// function dosumthing(num){
+//     console.log(`num is this iteration is ` + num);
+// if (num = 0){
+//     dosumthing(--num);
+// }
+// }
+// dosumthing(5)();
+
+// callback function
+// const greet = (name) =>{
+//         console.log(`hello ${name}`);
+// }
+// const sayhello = (greeting, name)=>{
+//     greeting(name);
+// };
+// sayhello(greet, "ali")
+
+
+// after some auto run this function
+// const greet = () =>{
+//         console.log(`hello ALi`);
+// }
+// setTimeout(greet,1000)
