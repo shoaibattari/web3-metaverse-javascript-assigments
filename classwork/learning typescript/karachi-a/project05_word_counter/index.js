2f775d860098724ec9f4d45297e1987edda35748
shoaib memon <shoaibattari83@gmail.com>
2022-12-25 01:15:10 +0500

    words and character counting app complete

--- classwork/learning typescript/karachi-a/project05_word_counter/index.js
+++ classwork/learning typescript/karachi-a/project05_word_counter/index.js
@@ -1,5 +1,8 @@
 import inquirer from "inquirer";
 import chalk from 'chalk';
+console.clear();
+console.log(chalk.greenBright("WELLCOME TO WORDS AND CHARACTER COUNTING APPLICATION"));
+console.log(chalk.greenBright("CREATE BY SHOAIB MEMON"));
 const userDataInput = await inquirer.prompt([
     {
         name: "userdata",
@@ -8,19 +11,26 @@ const userDataInput = await inquirer.prompt([
     }
 ]);
 let userData = userDataInput.userdata;
-console.log("userdata: " + userData);
-// //function create words count
+console.log;
+chalk.blueBright(("userdata: " + userData));
+// // //function create words count
 function wordsCounter() {
-    let wordsCounter = userData.trim().split(" ");
-    let dataClean = wordsCounter.filter(function (element) {
+    let wordsCounter = userData.split(" ").filter(function (element) {
         return element !== "";
     });
-    console.log(chalk.blueBright(`Total words Is:  ${dataClean.length}`));
+    console.log(chalk.blueBright(`Total words Is:  ${wordsCounter.length}`));
 }
 wordsCounter();
-// character counting
+// // function create character counting
 function characterCounter() {
-    let characterCounter = userData.trim();
-    console.log(chalk.blueBright(`Total Character Is: ${characterCounter.length}`));
+    let i = 0;
+    let newString = '';
+    while (i < userData.length) {
+        if (userData[i] != " ") {
+            newString = newString + userData[i];
+        }
+        i++;
+    }
+    console.log(chalk.blueBright(`Total Character Is: ${newString.length}`));
 }
 characterCounter();
--- classwork/learning typescript/karachi-a/project05_word_counter/index.ts
+++ classwork/learning typescript/karachi-a/project05_word_counter/index.ts
@@ -1,6 +1,11 @@
 import inquirer from "inquirer";
 import chalk from 'chalk';
 
+console.clear()
+console.log(chalk.greenBright("WELLCOME TO WORDS AND CHARACTER COUNTING APPLICATION"));
+console.log(chalk.greenBright("CREATE BY SHOAIB MEMON"));
+
+
 const userDataInput: { userdata: string } = await inquirer.prompt([
     {
         name: "userdata",
@@ -11,23 +16,28 @@ const userDataInput: { userdata: string } = await inquirer.prompt([
 
 let userData: string = userDataInput.userdata;
 
-console.log("userdata: " + userData);
+console.log
+chalk.blueBright(("userdata: " + userData));
 
-// //function create words count
+// // //function create words count
 
 function wordsCounter() {
-    let wordsCounter = userData.trim().split(" ");
-    let dataClean = wordsCounter.filter(function (element) {
+    let wordsCounter = userData.split(" ").filter(function (element) {
         return element !== ""
     })
-    console.log(chalk.blueBright(`Total words Is:  ${dataClean.length}`));
+    console.log(chalk.blueBright(`Total words Is:  ${wordsCounter.length}`));
 }
 wordsCounter()
 
-// character counting
-function characterCounter(){
-    
-let characterCounter = userData.trim();
-    console.log(chalk.blueBright(`Total Character Is: ${characterCounter.length}`));
+// // function create character counting
+function characterCounter() {
+    let i = 0;
+    let newString = ''
+    while (i < userData.length) {
+        if (userData[i] != " ") {
+            newString = newString + userData[i]
+        } i++}
+    console.log(chalk.blueBright(`Total Character Is: ${newString.length}`));
 }
 characterCounter()
+
