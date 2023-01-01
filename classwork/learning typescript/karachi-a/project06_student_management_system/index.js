1f98235675615cc17070d0c2da2c6680ceecf225
shoaib memon <shoaibattari83@gmail.com>
2023-01-02 00:06:44 +0500

    code add

--- classwork/learning typescript/karachi-a/project06_student_management_system/index.js
+++ classwork/learning typescript/karachi-a/project06_student_management_system/index.js
@@ -1,2 +1,73 @@
-console.log("hello");
+console.clear();
+class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getName() {
+        return this.name;
+    }
+}
+class Student extends Person {
+    rollNumber;
+    courses = [];
+    constructor(name, age, rollNumber) {
+        super(name, age);
+        this.rollNumber = rollNumber;
+    }
+    RegisterForCourses(course) {
+        this.courses.push(course);
+    }
+}
+class Instructor extends Person {
+    salary;
+    courses = [];
+    constructor(name, age, salary) {
+        super(name, age);
+        this.salary = salary;
+    }
+    ;
+    assignCourse(course) {
+        this.courses.push(course);
+    }
+}
+class Course {
+    id;
+    name;
+    students = [];
+    instructor = [];
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    ;
+    addStudent(student) {
+        this.students.push(student);
+        student.RegisterForCourses(this);
+    }
+    setInstructor(instructor) {
+        this.instructor.push(instructor);
+        instructor.assignCourse(this);
+    }
+}
+// student create
+const student1 = new Student("shoaib", 26, "st1");
+const student2 = new Student("sattar", 26, "st2");
+// instructor create
+const instructor1 = new Instructor("zia", 26, 25000);
+const instructor2 = new Instructor("daniyal", 26, 25000);
+//courses create
+const course1 = new Course("C1", "Metaverse");
+const course2 = new Course("C2", "blockChain");
+course1.addStudent(student1);
+course1.addStudent(student2);
+course1.setInstructor(instructor1);
+// console.log(course1);
+course2.addStudent(student1);
+course2.addStudent(student2);
+course2.setInstructor(instructor2);
+// console.log(course2);
+console.log(course1.instructor);
 export {};
--- classwork/learning typescript/karachi-a/project06_student_management_system/index.ts
+++ classwork/learning typescript/karachi-a/project06_student_management_system/index.ts
@@ -1,1 +1,101 @@
-console.log("hello");
+console.clear()
+
+// create class person
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    getName() {
+        return this.name
+    }
+}
+
+// create class student extende with person
+class Student extends Person {
+    rollNumber: string;
+    courses: Course[] = [];
+
+    constructor(name: string, age: number, rollNumber: string) {
+        super(name, age)
+        this.rollNumber = rollNumber
+    }
+    RegisterForCourses(course: Course) {
+        this.courses.push(course);
+    }
+}
+
+
+// create class instructor extende with person
+
+class Instructor extends Person {
+    salary: number;
+    courses: Course[] = [];
+
+
+    constructor(name: string, age: number, salary: number) {
+        super(name, age)
+        this.salary = salary;
+    };
+
+    assignCourse(course: Course) {
+        this.courses.push(course)
+    }
+}
+
+
+// create class course
+class Course {
+    id: string;
+    name: string;
+    students: Student[] = [];
+    instructor: Instructor[] = [];
+    constructor(id: string, name: string,) {
+        this.id = id;
+        this.name = name
+    };
+
+    addStudent(student: Student) {
+        this.students.push(student);
+        student.RegisterForCourses(this);
+    }
+
+    setInstructor(instructor: Instructor) {
+        this.instructor.push(instructor);
+        instructor.assignCourse(this)
+
+
+    }
+}
+
+
+
+
+// student create
+const student1 = new Student("shoaib", 26, "st1");
+const student2 = new Student("sattar", 26, "st2")
+
+
+// instructor create
+const instructor1 = new Instructor("zia", 26, 25000);
+const instructor2 = new Instructor("daniyal", 26, 25000);
+
+//courses create
+const course1 = new Course("C1", "Metaverse",)
+const course2 = new Course("C2", "blockChain",)
+
+
+// add student & instructor in course1
+course1.addStudent(student1)
+course1.addStudent(student2)
+course1.setInstructor(instructor1)
+
+
+// add student & instructor in course2
+course2.addStudent(student1);
+course2.addStudent(student2);
+course2.setInstructor(instructor2)
+
